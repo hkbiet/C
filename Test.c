2092b957a4509644be87d9ef845a038f7fd47404
hkbiet <hkbiet@gmail.com>
2018-11-09 17:43:38 +0530

    modified

--- Test.c
+++ Test.c
@@ -1,3 +1,12 @@
+/**
+ * Author : Hemant Kumar
+ * Repo : hkbiet/C
+ * Testing
+ *
+ */
+
+
+
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -34,57 +43,57 @@ int initialise(queue* q){
 //Function to check if a queue is empty
 int isempty(queue* q){
 
-if(q->end = q->front == NULL){
-	return 1;}else{
+	if(q->end = q->front == NULL){
+		return 1;}else{
 			return 0;
-			}
+		}
 
 }
 
 //Function to add an item/node to the queue.   -----------------------------------      ISSUE     ------------------------------------------------------------------------------
 int enqueue(queue* q){
 
-int user_value;
-char answer;
+	int user_value;
+	char answer;
 
-printf("\n Would you like to add an item into the queue \n");
-scanf("%c",&answer);
-//answer = getchar();
+	printf("\n Would you like to add an item into the queue \n");
+	scanf("%c",&answer);
+	//answer = getchar();
 
-while(answer == 'y'){
+	while(answer == 'y'){
 
-fflush(stdin);
-printf("\nType in a value to add to the queue: \n");
-scanf("%d",&user_value);
-fflush(stdin);
+		fflush(stdin);
+		printf("\nType in a value to add to the queue: \n");
+		scanf("%d",&user_value);
+		fflush(stdin);
 
-node_t * new_node = NULL;
-new_node = malloc(sizeof(node_t));
-new_node->value = user_value;
+		node_t * new_node = NULL;
+		new_node = malloc(sizeof(node_t));
+		new_node->value = user_value;
 
-if(isempty(q)){
+		if(isempty(q)){
 
-	//printf("\n Queue was empty !\n");
-	q->front = new_node;
-	q->end = new_node;
-	new_node->next = NULL;
+			//printf("\n Queue was empty !\n");
+			q->front = new_node;
+			q->end = new_node;
+			new_node->next = NULL;
 
-}else{
+		}else{
 
-	q->end->next = new_node;
-	q->end = new_node;
-	new_node->next = NULL;
+			q->end->next = new_node;
+			q->end = new_node;
+			new_node->next = NULL;
 
+		}
+
+		printf("\n Would you like to add an item into the queue \n");
+		//scanf("%[^\n]*c",&answer);
+		//gets(answer);
+		//answer = getchar();
+		scanf(" %c",&answer);
+		printf("\nUSER TYYPED IN %c\n",answer);
+		fflush(stdin);
 	}
-
-printf("\n Would you like to add an item into the queue \n");
-//scanf("%[^\n]*c",&answer);
-//gets(answer);
-//answer = getchar();
-scanf(" %c",&answer);
-printf("\nUSER TYYPED IN %c\n",answer);
-fflush(stdin);
-}
 }
 
 
@@ -96,27 +105,27 @@ int dequeue(){}
 int display(queue* q){
 
 	node_t * current_node = q->front;
-while(current_node!=NULL){
+	while(current_node!=NULL){
 
-	printf(">>%d",current_node->value);
-	current_node = current_node->next;
+		printf(">>%d",current_node->value);
+		current_node = current_node->next;
+	}
+
+	printf("\n End of the Queue Reached !\n");
 }
 
-printf("\n End of the Queue Reached !\n");
-}
-
-// The Main 
+// The Main
 
 int main(){
-queue* q = NULL;
-q = malloc((sizeof(queue)));
+	queue* q = NULL;
+	q = malloc((sizeof(queue)));
 
 	// Initialise the Queue
-initialise(q);
-//printf("%d",isempty(q));
+	initialise(q);
+	//printf("%d",isempty(q));
 
-enqueue(q);
-display(q);
+	enqueue(q);
+	display(q);
 }
 
 
